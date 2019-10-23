979a373da57bed2802aae120972327e805332981
Austin Dillow <austin.dillow@maymobility.com>
2019-10-22 23:08:39 -0400

    WIP compute part of controller

--- PID_Em.cpp
+++ PID_Em.cpp
@@ -6,10 +6,48 @@
 
 #include <PID_Em.h>
 
-void PID_Em::PID(float* input, float* output, float* setpoint, double kp_in, double ki_in, double kd_in) {
+void PID_Em::PID(float* input, float* output, float* setpoint_in, double kp_in, double ki_in, double kd_in) {
+    last_compute = millis();
+    setTuning(kp_in, ki_in, kd_in);
+}
 
+void PID_Em::compute() {
+    float dt = millis() - last_compute;
+    if (dt >= compute_period) {
+        // do the computation
+        double error = setpoint - *pidInput;
+        integral_sum += error * dt;
+        double result = error * kp + integral_sum * ki + (error - last_error);
+
+        last_input = *pidInput;
+        last_error = error;
+        last_compute = millis();
+        *pidOutput = result;
+    }
 }
 
 void PID_Em::setTuning(double kp_in, double ki_in, double kd_in) {
-    
+    kp = kp_in;
+    ki = ki_in;
+    kd = kd_in;
+}
+
+void PID_Em::setComputePeriod(int p) { ///< Period in ms between computes
+    if (p > 0) {
+        compute_period = p;
+    }
+}
+
+void PID_Em::newSetpoint(double setpoint_in) {
+    setpoint = setpoint_in;
+    last_compute = millis();
+}
+
+void PID_Em::printTuning() {
+    Serial.print("Kp = ");
+    Serial.println(kp);
+    Serial.print("Ki = ");
+    Serial.println(ki);
+    Serial.print("Kd = ");
+    Serial.println(kd);
 }
--- PID_Em.h
+++ PID_Em.h
@@ -2,14 +2,23 @@
 #define PID_h
 
 class PID_Em {
-    public:
-        void PID(float* input, float* output, float* setpoint, double kp_in, double ki_in, double kd_in);
-        void setTuning(double kp_in, double ki_in, double kd_in);
+public:
+  void PID(float *input, float *output, float *setpoint_in, double kp_in, double ki_in, double kd_in);
+  void compute();
+  void setTuning(double kp_in, double ki_in, double kd_in);
+  void printTuning();
+  void newSetpoint(double setpoint_in);
+  void setComputePeriod(int p);
 
-    private:
-
-    double Kp, Ki, Kd;
+private:
+  double kp, ki, kd;
+  double setpoint;
+  double integral_sum; // store the integral portion of the controller
+  int compute_period = 50; // how often the PID controller computes (if called more frequently)
+  double last_input, last_output, last_error;
+  unsigned long last_compute;
+  double *pidInput;
+  double *pidOutput;
 };
-
 
 #endif
