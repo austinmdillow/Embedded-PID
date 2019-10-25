52c876eb822d1c20285263402279c92f536eb83f
austinmdillow <austinmdillow@gmail.com>
2019-10-24 23:12:44 -0400

    added anti windup info

--- PID_Em.cpp
+++ PID_Em.cpp
@@ -5,25 +5,47 @@
   #endif
 
 #include <PID_Em.h>
+#include <math.h>
 
 // TODO add error checking 
 
-void PID_Em::PID(float* input, float* output, float* setpoint_in, double kp_in, double ki_in, double kd_in) {
+PID_Em::PID_Em(double* input, double* output, double* setpoint_in, double kp_in, double ki_in, double kd_in) {
     last_compute = millis();
     setTuning(kp_in, ki_in, kd_in);
     setOutputContraints(0, 255); // limits of Arduino DAC
 }
 
+/**
+ * Perform the PID calculation and sets output value.
+ * This should be called in a loop and more frequently than the update period
+ */
 void PID_Em::compute() {
     double dt = millis() - last_compute;
     if (dt >= compute_period) {
+        if (dt >= 1.5 * compute_period) {
+            error_codes = PID_Stall;
+        }
         // do the computation
         dt = dt / 1000; // convert delta T to seconds
         double error = setpoint - *pidInput;
+        
+        // checks for integral windup
+        if (anti_windup) {
+            if (fabs(error) > integral_domain) {
+                integral_sum += error * dt;
+                if (integral_sum > integral_max) {
+                    integral_sum = integral_max;
+                } else if (integral_sum < -integral_max) {
+                    integral_sum = -integral_max;
+                }
+            }
+        } else {
+            integral_sum += error * dt;
+        }
+
         integral_sum += error * dt;
         double derrivative = (error - last_error) / dt;
         double result = error * kp + integral_sum * ki + derrivative * kd;
-
 
         // Check that the result is within the give contraints
         if (result > maxOut) {
@@ -33,7 +55,6 @@ void PID_Em::compute() {
         }
 
         *pidOutput = result;
-
         last_error = error;
         last_compute = millis();
         
@@ -46,22 +67,48 @@ void PID_Em::setTuning(double kp_in, double ki_in, double kd_in) {
     kd = kd_in;
 }
 
+/**
+ * Sets the time between PID computations
+ * @param p time in ms
+ */
 void PID_Em::setComputePeriod(int p) { ///< Period in ms between computes
     if (p > 0) {
         compute_period = p;
     }
 }
 
+/**
+ * Constrains the output of the controller to the given range
+ * @param min the lower bound
+ * @param max the upper bound
+ */
 void PID_Em::setOutputContraints(double min, double max) {
     minOut = min;
     maxOut = max;
 }
 
+/**
+ * Controls the anit-windup portion of the controller.
+ * @param maxIntegral how large the integral part can become in the output range
+ * @param integrationDomain how far (either side of the setpoint) the integration component will continue to add
+ */
+void PID_Em::setIntegralWindup(double maxIntegral, double integrationDomain) {
+    integral_max = maxIntegral;
+    integral_domain = integrationDomain;
+    anti_windup = true;
+}
+
+void PID_Em::resetIntegralWindup() {
+    anti_windup = false;
+}
+
 void PID_Em::newSetpoint(double setpoint_in) {
-    setpoint = setpoint_in;
-    last_compute = millis();
-    // TODO alleviate problems of high derrivative controller
-    integral_sum = 0; // reset the integral controller
+    if (setpoint != setpoint_in) { // if there is no change, do nothing
+        setpoint = setpoint_in;
+        last_compute = millis();
+        // TODO alleviate problems of high derrivative controller
+        integral_sum = 0; // reset the integral controller
+    }
 }
 
 void PID_Em::printTuning() {
